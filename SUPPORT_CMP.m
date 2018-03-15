d8362119e7748d16eec6f22430d8828882491ab1
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-15 16:37:27 +0000

    Fix unused vars

--- SUPPORT_CMP.m
+++ SUPPORT_CMP.m
@@ -5,12 +5,10 @@ function [difference] = SUPPORT_CMP(image1, image2, window_size)
 image1 = imread(image1);
 image2 = imread(image2);
 
-window = [1:window_size, 1:window_size];
-
 image_1_window = (image1(1:window_size, 1:window_size));
 image_2_window = (image2(1:window_size, 1:window_size));
 
-difference = sum((image_1_window(:) - image_2_window(:)).^2 );
+% Compute difference with SSD (Sum of squared differences)
+difference = sumsqr( image_1_window(:) - image_2_window(:) );
 
 end
-
