80f5ad72c5ffcbfb5f40335a721875a44f230e79
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-15 22:21:40 +0000

    Add template files

---
+++ DISP_MAP.m
@@ -0,0 +1,9 @@
+function DISP_MAP(image_1, image_2, output_image, support_window_size)
+% Read image files and convert to grayscale
+image_1 = rgb2gray(imread(image_1));
+image_2 = rgb2gray(imread(image_2));
+
+
+
+end
+
--- SUPPORT_CMP.m
+++ SUPPORT_CMP.m
@@ -1,14 +1,4 @@
-function [difference] = SUPPORT_CMP(image1, image2, window_size)
-%SUPPORT_CMP Summary of this function goes here
-%   Detailed explanation goes here
-
-image1 = imread(image1);
-image2 = imread(image2);
-
-image_1_window = (image1(1:window_size, 1:window_size));
-image_2_window = (image2(1:window_size, 1:window_size));
-
-% Compute difference with SSD (Sum of squared differences)
-difference = sumsqr( image_1_window(:) - image_2_window(:) );
-
+function [difference] = SUPPORT_CMP(matrix_1, matrix_2)
+% Compute difference with SSD (Sum of squared differences) of two matrices
+difference = sumsqr( matrix_1(:) - matrix_2(:) );
 end
