464f2eebd51a51e6264947c4896e4f19ec7dbbe1
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-16 16:13:13 +0000

    Update PIXEL_DISP.m

--- PIXEL_DISP.m
+++ PIXEL_DISP.m
@@ -1,7 +1,18 @@
-function [outputArg1,outputArg2] = PIXEL_DISP(inputArg1,inputArg2)
-%PIXEL_DISP Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [disp] = PIXEL_DISP(ref_pixel, ins_mat)
+
+height = size(ins_mat, 1);
+width = size(ins_mat, 2);
+
+SMP_vals = zeros(size(ins_mat));
+
+for i=1:height
+    for j=1:width
+        SMP_vals(i,j) = SUPPORT_CMP(ref_pixel, ins_mat(i,j));
+    end
 end
 
+SMP_vals = reshape(SMP_vals, [1, height*width]);
+
+disp = min(abs(SMP_vals));
+
+end
