9d07cdfc0bd12b8461f4a657dba539a435b6e49a
ijm7 <ijmorton500@gmail.com>
2018-03-16 16:12:31 +0000

    Add initial disparity map

--- DISP_MAP.m
+++ DISP_MAP.m
@@ -1,9 +1,16 @@
 function DISP_MAP(image_1, image_2, output_image, support_window_size)
-% Read image files and convert to grayscale
+%% Read image files and convert to grayscale
 image_1 = rgb2gray(imread(image_1));
 image_2 = rgb2gray(imread(image_2));
-
-
-
+%% Call PIXEL_DISP on each pixel
+for i = 1:size(image_1, 1)
+    for j = 1:size(image_1, 2)
+        support_window = image_2(i - support_window_size: i + support_window_size, i - support_window_size: i + support_window_size);
+        image_1(i,j) = PIXEL_DISP(image(i,j), support_window);
+    end
+end
+%% Show image
+figure
+imshow(image_1)
 end
 
