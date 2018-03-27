6b7781d878203d308b435c1cee261ee3a4574c6d
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-27 18:11:42 +0100

    Noisey map

--- DISP_MAP.m
+++ DISP_MAP.m
@@ -9,11 +9,13 @@ else
 end
 %% Iterate over left and right images, generating a disparity map from left to right with left as reference image
 [R, C, ~] = size(left_image); % Assumes both images are the same size
-dmap_image = uint8(zeros(R, C)); % Create empty/zeroed matrix of same size as input images
 
 % Pad our images with zeros to avoid out-of-bounds errors
 padded_reference_image = padarray(left_image,[support_window_size,support_window_size], 'both');
-padded_nonreference_image = padarray(left_image,[search_region_size,search_region_size], 'both');
+padded_nonreference_image = padarray(right_image,[search_region_size,search_region_size], 'both');
+
+dmap_image = uint8(zeros(R, C)); % Create empty/zeroed matrix of same size as input images
+padded_dmap_image = padarray(dmap_image,[support_window_size,support_window_size], 'both');
 
 % Compute our search and support size differences
 search_support_diff = search_region_size - support_window_size;
@@ -26,10 +28,20 @@ for r = (1+support_window_size):(R+support_window_size) % Row
         
         % Define our support window as a segment of our reference image
         support_window = padded_reference_image(r:r, c:c+support_window_size-1);
+        [H, W, ~] = size(support_window);
         % Define our search region as a segment of our non-reference image
         search_region = padded_nonreference_image(r+search_support_diff:r+search_support_diff, c+search_support_diff:c+search_support_diff+search_region_size-1);
         
+        for i = 1:H
+            for j = 1:W
+                padded_dmap_image(r,c) = PIXEL_DISP(support_window(i,j), search_region);
+            end
+        end
     end
 end
+
+normalisedImage = uint8(255*mat2gray(padded_dmap_image));
+
+imshow(normalisedImage);
 dmap_image = 1;
 end
