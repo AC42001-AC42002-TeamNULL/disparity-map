b55cf182939079be866eab9a128c9f4474bd77fa
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-27 16:09:11 +0100

    remove misc files

--- CHECK_LOWER_BOUND.m
+++
@@ -1,8 +0,0 @@
-function [bound] = CHECK_LOWER_BOUND(input, maxsize)
-if input > 0
-    bound = input;
-else
-    bound = 1;
-end
-end
-
--- CHECK_UPPER_BOUND.m
+++
@@ -1,9 +0,0 @@
-function [bound] = CHECK_UPPER_BOUND(input, max)
-if input <= max
-    
-    bound = input;
-else
-    bound = max;
-end
-end
-
