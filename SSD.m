e55103282125dc97af52e0e5df7bdfc2079257e4
Anthony Grimes <contact@anthonygrimes.co.uk>
2018-03-29 18:10:04 +0100

    sliding window upload

--- SSD.m
+++ SSD.m
@@ -1,4 +1,4 @@
 function [ssd] = SSD(matrix_1, matrix_2)
 % Compute difference with SSD (Sum of squared differences) of two matrices
-ssd = sumsqr( matrix_1(:) - matrix_2(:) );
+ssd = 0 - sumsqr( matrix_1(:) - matrix_2(:) );
 end
